c1570a4726216d50ee2b9b388291d4dbc6bd33fc
NAGANDLA VENU GOPAL <31363808+VG1234@users.noreply.github.com>
2018-08-28 17:38:10 +0530

    Add files via upload

---
+++ mainpage.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<center><h2>Welcome to Student details section</h2></center><br>
+<?php include 'Manage.php'; ?><br>
+<p>Insert the details</p><br>
+<?php include 'insert1.php'; ?>
+<p>List all details of studentdetails</p><br>
+<?php include 'databs.php'; ?>
+</body>
+</html>
